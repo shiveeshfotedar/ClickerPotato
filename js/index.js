a91a59bbfdb6a31084f328fd4338bf64c3d7222b
Shiveesh Fotedar <shiveeshfotedar@gmail.com>
2022-04-10 00:20:23 -0700

    added selling doubleClicker

--- js/index.js
+++ js/index.js
@@ -2,15 +2,25 @@ let potatoes = 0 ;
 let addPotato = document.getElementById('potato')
 let increment = 1
 let score = document.querySelector("#score")
+let moneyValue = document.querySelector('#money')
 
+let money = 0
+
+addPotato.onmousedown = function(){
+    addPotato.style.transform = "scale(0.9)"
+}
+
+addPotato.onmouseup = function(){
+    addPotato.style.transform = "scale(1)"
+}
 function addPotatoes(base, adder){
     return base + adder
 }
 
 function sellPotatoes(base, adder){
+    money = money + adder
     return base - adder
 }
-
 
 function clickedPotato(){
     potatoes = addPotatoes(potatoes, increment)
@@ -21,13 +31,17 @@ function clickedPotato(){
 function soldPotato(){
     potatoes = sellPotatoes(potatoes, increment)
     score.innerHTML = potatoes
+    moneyValue.innerHTML = money
     console.debug(potatoes)
 }
 
-addPotato.onmousedown = function(){
-    addPotato.style.transform = "scale(0.8)"
+// Show the double clicker after some money. 
+
+
+function buyDoubleClicker(){
+    money = money - 100
+    moneyValue.innerHTML = money
+    increment = increment*2
 }
 
-addPotato.onmouseup = function(){
-    addPotato.style.transform = "scale(1)"
-}
+
