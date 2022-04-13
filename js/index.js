794e5ed6022112f4bb18e6f042105633f5eaa044
Shiveesh Fotedar <fotedars@amazon.com>
2022-04-13 16:46:25 -0700

    finsihed it

--- js/index.js
+++ js/index.js
@@ -6,42 +6,75 @@ let moneyValue = document.querySelector('#money')
 
 let money = 0
 
-addPotato.onmousedown = function(){
-    addPotato.style.transform = "scale(0.9)"
-}
+    addPotato.onmousedown = function(){
+        addPotato.style.transform = "scale(0.9)"
+    }
+    
+    addPotato.onmouseup = function(){
+        addPotato.style.transform = "scale(1)"
+    }
+    function addPotatoes(base, adder){
+        return base + adder
+    }
+    
+    function sellPotatoes(base, adder){
+        if(base > 0){
+            money = money + adder
+            return base - adder
+        }else{
+            return 0
+        }
+    }
+    
+    function clickedPotato(){
+        potatoes = addPotatoes(potatoes, increment)
+        score.innerHTML = potatoes
+        console.debug(potatoes)
+        if(potatoes > 10000){
+            alert(" No more potatoes to get")
+            potatoes = 0 
+            moneyValue = 0
+            score.innerHTML = potatoes
+        }
+    }
+    
+    function soldPotato(){
+        potatoes = sellPotatoes(potatoes, increment)
+        score.innerHTML = potatoes
+        moneyValue.innerHTML = money
+        console.debug(potatoes)
+    }
+    
+    // Show the double clicker after some money. 
+    
+    
+    function buyDoubleClicker(){
+        if(money > 99) {
+            money = money - 100
+            moneyValue.innerHTML = money
+            increment = increment*2
+            document.querySelector("#BuyDoubleClicker").style.display = "none"
+        }
+    }
+    
+    function buyTripleClicker(){
+        if(money > 999){
+            money = money - 1000
+            moneyValue.innerHTML = money
+            increment = increment*3
+            document.querySelector("#BuyTripleClicker").style.display = "none"
+        }
+    }
+    
+    function buyTenClicker(){
+        if(money > 9999){
+            money = money - 100000
+            moneyValue.innerHTML = money
+            increment = increment*10
+            document.querySelector("#BuyTenClicker").style.display = "none"
+        }
+    }
 
-addPotato.onmouseup = function(){
-    addPotato.style.transform = "scale(1)"
-}
-function addPotatoes(base, adder){
-    return base + adder
-}
 
-function sellPotatoes(base, adder){
-    money = money + adder
-    return base - adder
-}
-
-function clickedPotato(){
-    potatoes = addPotatoes(potatoes, increment)
-    score.innerHTML = potatoes
-    console.debug(potatoes)
-}
-
-function soldPotato(){
-    potatoes = sellPotatoes(potatoes, increment)
-    score.innerHTML = potatoes
-    moneyValue.innerHTML = money
-    console.debug(potatoes)
-}
-
-// Show the double clicker after some money. 
-
-
-function buyDoubleClicker(){
-    money = money - 100
-    moneyValue.innerHTML = money
-    increment = increment*2
-}
 
 
